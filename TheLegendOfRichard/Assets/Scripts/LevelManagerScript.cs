f9e0f1c8d71d6381b7cb8e37dd696bc30c308ba5
JackOS <60794490+JackOSullivan003@users.noreply.github.com>
2024-11-12 22:04:45 +0000

    level manager v.0.1 and vscode .gitignore

--- TheLegendOfRichard/Assets/Scripts/LevelManagerScript.cs
+++ TheLegendOfRichard/Assets/Scripts/LevelManagerScript.cs
@@ -1,18 +1,72 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEditor.Search;
 using UnityEngine;
 
 public class LevelManagerScript : MonoBehaviour
 {
+    [SerializeField] List<GameObject> enemies;
+    [SerializeField] List<GameObject> currentEnemies;
+    [SerializeField] Vector3[] spawnPoints;
+    [SerializeField] int totalNumberOfEnemies;
+    [SerializeField] GameObject player;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //start spawning enemies and keep track of how many are alive
+        if(player == null){
+            // //try get player until player is not null
+            // while(player == null){
+                player = GameObject.FindWithTag("Player");
+            //}
+        }
+        StartCoroutine(SpawnEnemies());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator SpawnEnemies()
     {
+        for (int i = 0; i <= totalNumberOfEnemies; i++) //
+        {
+            //choose a random spawn point
+            int randomIndex = UnityEngine.Random.Range(0, spawnPoints.Length);
+            Vector3 spawnPoint = spawnPoints[randomIndex];
+            
+            //create an enemy at the chosen spawn point
+            GameObject enemy = Instantiate(enemies[UnityEngine.Random.Range(0, enemies.Count)], spawnPoint, Quaternion.identity);
+            //add enemy to list of current enemies 
+            currentEnemies.Append(enemy);
+            
+            yield return new WaitForSeconds(1f);
+        }
         
+        //all enemies have been spawned, finish Coroutine 
+        StopCoroutine(SpawnEnemies());
+    }
+
+    public void EnemyDied(GameObject enemy)
+    {
+        //remove enemy from list of current enemies
+        currentEnemies.Remove(enemy);
+        
+        //check if all enemies are dead
+        if (currentEnemies.Count == 0)
+        {
+            //all enemies are dead, Win State
+            Debug.Log("All enemies are dead!");
+        }
+
+    }
+
+    public void PlayerDied()
+    {
+        //Player died, Game Over State
+        Debug.Log("Player died!");
+        //stop spawning enemies
+        StopCoroutine(SpawnEnemies());
     }
 }
