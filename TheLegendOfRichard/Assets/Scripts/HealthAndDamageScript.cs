72af8cc5b511c581256d378a4adac42957b72e99
JackOS <60794490+JackOSullivan003@users.noreply.github.com>
2024-11-13 12:56:15 +0000

    created Player Prefab + Adjusted H&D Script
    Also added Player Tag

--- TheLegendOfRichard/Assets/Scripts/HealthAndDamageScript.cs
+++ TheLegendOfRichard/Assets/Scripts/HealthAndDamageScript.cs
@@ -23,7 +23,7 @@ public class HealthAndDamageScript : MonoBehaviour
         
         if(health <= 0){
 
-            if(gameObject.tag == "enemy"){
+            if(gameObject.tag == "enemy" || gameObject.tag == "EnemyTrash"){
                 //if is enemy, call LevelManager enemy died method 
                 levelManager.EnemyDied(this.gameObject);
             }
