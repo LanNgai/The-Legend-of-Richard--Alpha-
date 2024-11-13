7c835eacc5b43e7d23555129c92e408649e04f82
Lan Ngai <b00159127@mytudublin.ie>
2024-11-13 23:19:04 +0000

    Trying to add a small push

--- TheLegendOfRichard/Assets/Scripts/EnemyScript.cs
+++ TheLegendOfRichard/Assets/Scripts/EnemyScript.cs
@@ -7,6 +7,9 @@ public class EnemyScript : MonoBehaviour
     public float speed;
     private Rigidbody enemyRb;
     private GameObject player;
+
+    private float attackRange = 3.0f;
+    private float boost = 10.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,18 +19,30 @@ public class EnemyScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         player = GameObject.Find("Player");
         Vector3 lookDirection = (player.transform.position - transform.position).normalized;
-        enemyRb.transform.Translate(lookDirection * speed * Time.deltaTime);
+        if (Vector3.Distance(player.transform.position, transform.position) > attackRange)
+        {
+            enemyRb.transform.Translate(lookDirection * speed * Time.deltaTime);
+        }else if (Vector3.Distance(player.transform.position, transform.position) <= attackRange)
+        {
+            enemyRb.AddForce(lookDirection * boost, ForceMode.Impulse);
+        }
     }
     
     //TODO: name of player projectile
     private void OnCollisionEnter(Collision other)
     {
+        Vector3 lookDirection = (player.transform.position - transform.position).normalized;
         // If enemy gets hit by HairBall it will be destroyed
         if (other.gameObject.name == "HairBall")
         {
             Destroy(gameObject);
-        } 
+        }else if (other.gameObject.name == "Player")
+        {
+            enemyRb.transform.Translate(-lookDirection * speed * Time.deltaTime);
+        }
     }
+    
 }
