190888bd7f5786cefbc8b0bd24315f36998da718
Lan Ngai <b00159127@mytudublin.ie>
2024-11-13 00:20:42 +0000

    EnemyScript
    Enemies are falling through the floor

--- TheLegendOfRichard/Assets/Scripts/EnemyScript.cs
+++ TheLegendOfRichard/Assets/Scripts/EnemyScript.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class EnemyScript : MonoBehaviour
 {
+    public float speed;
+    private Rigidbody enemyRb;
+    private GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        enemyRb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        player = GameObject.Find("Player");
+        Vector3 lookDirection = (player.transform.position - transform.position).normalized;
+        enemyRb.AddForce(lookDirection * speed * Time.deltaTime);
+    }
+    
+    //TODO: name of player projectile
+    private void OnCollisionEnter(Collision other)
+    {
+        // If enemy collides with either goal, destroy it
+        if (other.gameObject.name == "Hairball")
+        {
+            Destroy(gameObject);
+        } 
     }
 }
