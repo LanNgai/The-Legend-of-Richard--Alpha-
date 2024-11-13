38590145c74122d1510aef45a44d824fc5fa067d
Lan Ngai <b00159127@mytudublin.ie>
2024-11-13 23:51:09 +0000

    Fixed Menu not found and prefabs missing

--- TheLegendOfRichard/Assets/Scripts/HairballScript.cs
+++ TheLegendOfRichard/Assets/Scripts/HairballScript.cs
@@ -6,11 +6,10 @@ public class Hairball : MonoBehaviour
 {
     public float speed = 40;
     public int damageAmount = 20;
-    public float time = 2f;
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(gameObject, time);
+        
     }
 
     // Update is called once per frame
