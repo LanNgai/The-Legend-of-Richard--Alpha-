aead3ade5f2d87dde14a6d1090df3ce1898e3bee
Fausta Pavalkyte <B00165884@mytudublin.ie>
2024-11-15 20:12:20 +0000

    level 1 in proggress

--- TheLegendOfRichard/Assets/Scripts/HairballScript.cs
+++ TheLegendOfRichard/Assets/Scripts/HairballScript.cs
@@ -4,10 +4,11 @@ public class Hairball : MonoBehaviour
 {
     public float speed = 40;
     public int damageAmount;
+    public float time;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Destroy(gameObject, time);
     }
 
     // Update is called once per frame
