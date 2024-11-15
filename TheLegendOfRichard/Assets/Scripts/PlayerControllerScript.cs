9ee187f4b67a1ad3edde58acb4c3864e57c54b64
JackOS <60794490+JackOSullivan003@users.noreply.github.com>
2024-11-15 15:31:17 +0000

    Player Aiming with mouse and damage system implemented

--- TheLegendOfRichard/Assets/Scripts/PlayerControllerScript.cs
+++ TheLegendOfRichard/Assets/Scripts/PlayerControllerScript.cs
@@ -7,15 +7,19 @@ public class PlayerMovement : MonoBehaviour
     public GameObject projectilePrefab;
     private float fireRate = 0.5f;
     private float nextFire = 0;
-    public float horizontalInput;
-    public float verticalInput;
+    private float horizontalInput;
+    private float verticalInput;
     public float speed;
-    public float rotationSpeed;
+
+    Camera cam; 
+    Ray ray;
+    RaycastHit hit;
+    Vector3 Hitpoint = Vector3.zero;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        cam = Camera.main;
     }
 
     // Update is called once per frame
@@ -23,13 +27,26 @@ public class PlayerMovement : MonoBehaviour
     {
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
+        
+        // Move the player based on the horizontal and vertical inputs
+        transform.rotation = Quaternion.identity;
+        transform.Translate(Vector3.forward * verticalInput * speed * Time.deltaTime);
+        transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
+        
+        if(cam){
+            ray = cam.ScreenPointToRay(Input.mousePosition); //creating a ray from where the mouse is on the screen into the game world
 
-        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
-        transform.Rotate(Vector3.up * horizontalInput * Time.deltaTime * rotationSpeed);
+                //if shooting the ray results in a hit...
+                if (Physics.Raycast(ray, out hit)){
 
+                    Hitpoint = hit.point; //getting the point of intersection of the raycast and what it hits 
+                    transform.LookAt(new Vector3(Hitpoint.x, transform.position.y, Hitpoint.z)); //rotate the player to face in the direction of the hitpoint, keeping its y rotate so it doesnt aim down or up.
+                }
+        }
+        
 
         // On spacebar press, if enough time has passed since last fire, shoot hairball in the direction the player is facing
-        if (Input.GetKeyDown(KeyCode.Space) && Time.time > nextFire)
+        if (Input.GetKeyDown(KeyCode.Mouse0) && Time.time > nextFire)
         {
             nextFire = Time.time + fireRate;// reset nextFire to current time + fireRate
             Vector3 projectileSpawnPosition = transform.position + (transform.forward * 3);
